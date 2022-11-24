a93e673e285e29822dba3a7cc2ed99fc371174b6
ErJump <fedelijunior@gmail.com>
2022-11-24 17:16:26 +0100

    feat: :sparkles: Ex. completed
    Adds CalcolatriceTest class with test methods

---
+++ src/test/java/org/generation/italy/CalcolatriceTest.java
@@ -0,0 +1,38 @@
+package org.generation.italy;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CalcolatriceTest {
+	private Calcolatrice c = new Calcolatrice();
+	
+	@Test
+	@DisplayName("Test addizione")
+	void addTest() {
+		float value = c.add(1, 2);
+		assertEquals(3, value);
+	}
+	
+	@Test
+	@DisplayName("Test sottrazione")
+	void subracTest() {
+		float value = c.subtract(4, 10);
+		assertEquals(-6, value);
+	}
+	
+	@Test
+	@DisplayName("Test moltiplicazione")
+	void multiplyTest() {
+		float value = c.multiply(4, 10);
+		assertEquals(40, value);
+	}
+	
+	@Test
+	@DisplayName("Test divisione")
+	void divideTest() {
+		float value = c.divide(4, 10);
+		assertEquals(0.4f, value);
+	}
+}
